4a71d45f51b1bb4b5c5a3316cd2558907a780ed0
scientiest35 <ibomyakmc@hotmasdail.com>
2018-12-08 23:17:18 +0300

    Add project files.

---
+++ firstP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace firstP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=5; i++)
+            {
+                for (int j = 1; j <=5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
